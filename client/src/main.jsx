c4b5f60fb09b2f9763eb10fecaf7e072a574c73c
Bilal-Sweidan <bilalsweidan58@gmail.com>
2024-07-23 11:52:31 +0300

    23/7/2024

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+
+// Components
+import Home from './pages/Home.jsx'
+
+// Views
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>
-  }
+    element: <Home/>
+  },
+
+
 ])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Link,Outlet } from "react-router-dom";
+import './style/Home.scss'
+function Home(){
+    return(
+        <>
+            <header className="main-header">
+                <div className="right-div text-bg-dark p-2">
+                    lightBooks
+                </div>
+                <div className="left-div">
+                    
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Home;
